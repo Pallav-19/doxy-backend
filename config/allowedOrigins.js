c04dd1bf59cbe3d535315c984d5535814174a927
Pallav Kumar Patra <73886524+Pallav-19@users.noreply.github.com>
2024-07-21 11:44:29 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    "https://doxy-frontend.vercel.app"
+    "https://doxy-frontend.vercel.app",
+    "https://doxy.pallav.site",
 ]
 export default allowedOrigins
